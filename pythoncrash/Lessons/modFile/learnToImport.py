4e3855567b2b64edfb82bc39743e818691e6dd2d
hackerFaruk <onuralparslan97@gmail.com>
2024-04-07 16:10:14 +0300

    created new files

---
+++ pythoncrash/Lessons/modFile/learnToImport.py
@@ -0,0 +1,9 @@
+from testModule.sound.speed import speedSound
+
+from testModule.color.colorDetect import color as clr
+
+from testModule.color.fancy.colorFancy import fancyColor
+
+speedSound()
+clr()
+fancyColor()
