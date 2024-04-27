8f81a1731957098cb14b9e50e452f3e5a3644c58
hackerFaruk <onuralparslan97@gmail.com>
2024-04-27 21:24:21 +0300

    project made

---
+++ tictactoeProject/gameEngine.py
@@ -0,0 +1,54 @@
+from player import Player 
+
+# board 
+# protokol 
+# a= [[1,2,3],[4,5,6],[7,8,9]]
+
+ # boş kare =  
+
+
+class GameEngine:
+    empty = 0 
+    player1Move= 1
+    player2Move=  2
+
+
+
+    def __init__(self, player1=None, player2=None):
+        self.player1 = player1
+        self.player2 = player2
+
+        if (player1 is None or player2 is None):
+            print("Not Enough Players")
+            raise Exception("Not Enough Players For Game Engine please provide 2 players")
+
+    gameState=[[ empty, empty, empty],
+               [ empty, empty, empty],
+               [ empty, empty, empty]]
+
+
+    
+    def display(self)->None:
+        # displays current game 
+        for i in self.gameState:
+            print(i)
+
+ 
+    def playerMoveRequest(self) -> None:
+        moveTuple=self.player1.PlayerMove()
+        
+        pass
+
+    def turnStalker(): 
+        pass
+        
+    def winDetection(self)->None:
+        pass
+
+
+
+
+if __name__ == "__main__":
+
+    testEngine=GameEngine()
+    testEngine.display()
---
+++ tictactoeProject/player.py
@@ -0,0 +1,42 @@
+
+class Player:
+
+    def __init__(self,name):
+        self.name = name
+
+    unknown = 0
+
+    moveLog=  [[ unknown, unknown, unknown],
+               [ unknown, unknown, unknown],
+               [ unknown, unknown, unknown]]
+
+    enemyMoves =[[ unknown, unknown, unknown],
+               [ unknown, unknown, unknown],
+               [ unknown, unknown, unknown]]
+
+    gameState= [[ unknown, unknown, unknown],
+               [ unknown, unknown, unknown],
+               [ unknown, unknown, unknown],]
+    
+
+
+   
+    def playerMove(self):
+        print(f"Hamle Sırası Sende {self.name} ")
+        
+        move=input("sol üst 1 olacak şekilde seçtiğiniz kare numarsını veriniz ")
+        move=int(move)
+        move -=1
+        moveA= move/3
+        moveB = move%3
+        moveTuple=(moveA, moveB)
+        return moveTuple
+
+
+
+
+# burdan aşağsı importta çalışmaz 
+if __name__ == "__main__":
+
+
+    print("test gerçek kod değil")
